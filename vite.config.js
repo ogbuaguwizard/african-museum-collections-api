568be481cb6b772130384c1dfb68343218babd6b
ogbuaguwizard <hakerfranck6@gmail.com>
2026-07-22 18:42:59 +0100

    fix: force https in vite in prod

--- vite.config.js
+++ vite.config.js
@@ -17,6 +17,7 @@ export default defineConfig({
         tailwindcss(),
     ],
     server: {
+        https: true,
         watch: {
             ignored: ['**/storage/framework/views/**'],
         },
